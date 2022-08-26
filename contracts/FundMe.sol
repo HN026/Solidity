e7f04bfe899bb8120869f43e2d7285a8b6053ec5
Huzaifa Naseer <101128679+HN026@users.noreply.github.com>
2022-08-26 22:29:45 +0530

    Update FundMe.sol

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -18,8 +18,11 @@
 pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe{
+
+    using PriceConverter for uint256;
 
     uint256 public minimumUsd = 50 * 1e18;
 
@@ -31,53 +34,14 @@ contract FundMe{
          // We want to be able to set a minimum fund amount in USD
         //  1. How do we send eth to this contract??
         // The payable keyword makes the function payable.
-        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough funds");    //1e18 = 1*10**18  The value of minimum fund is 1eth
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough funds");    //1e18 = 1*10**18  The value of minimum fund is 1eth
+        // Above, msg.value get considered to be the first parameter hence we don't need to put the value in brackets
 
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
-
-
-
      }
 
-      function getPrice() public view returns (uint256){
-          // Since this is an instance of us interacting with a contract outside of our project
-          // hence we will be needing two things: 
-          // 1. ABI 
-          // 2. ADDRESS OF THE CONTRACT - 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-           AggregatorV3Interface priceFeed = AggregatorV3Interface( 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-           (,int256 price,,,) = priceFeed.latestRoundData();
-           // This is going to be price of eth in terms of USD
-           // We multiplied by 1e10 because we wanted the price to have in total of 18 decimal places.
-           // Solidity doesnt work with decimals hence we are using wei therefore we will need to multiply 
-           // the esegements to ease the calculations..
 
-           return uint256(price * 1e10); // 1**10 = 10000000000
-
-
-
-
-
-
-      }
-
-
-
-
-        function getVersion() public view returns (uint256) {
-            AggregatorV3Interface priceFeed = AggregatorV3Interface( 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-            return priceFeed.version();
-        }
-
-
-
-
-      function getConversionRate(uint256 ethAmount) public view returns (uint256){
-          uint256 ethPrice = getPrice();
-          uint256 ethAmountInUsd = (ethPrice * ethAmount)/1e18;
-          return ethAmountInUsd;
-
-      }
 
 
     //  function Withdraw(){}
